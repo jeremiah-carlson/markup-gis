e9476b467c99ca30aa8ac7e997a7950dd0e6ea01
jeremiah-carlson <carlsonjeremiah2@gmail.com>
2023-08-20 01:53:26 -0400

    Fix map tile render

--- web/static/src/components/Map.jsx
+++ web/static/src/components/Map.jsx
@@ -5,13 +5,15 @@ import { onMount } from "solid-js";
 const Map = ()=>{
 
     onMount(() => {
-        const map = L.map('map-div').setView([51.505, -0.09], 13);;
+        const map = L.map('map-div').setView([51.505, -0.09], 4);;
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
+
+        L.control.mousePosition().addTo(map);
     })
-    return <div id="map-div" style="height:300px" class="z-20"></div>
+    return <div id="map-div" class="h-3/4 w-full z-20"></div>
 };
 
 export default Map;
