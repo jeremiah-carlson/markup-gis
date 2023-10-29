5171a123e8bb0a6bc578581b59e5f7f398caf6a4
Jeremiah Carlson <87202597+jeremiah-carlson@users.noreply.github.com>
2023-10-29 01:01:54 -0400

    Test marker add/remove

--- web/static/src/components/Map.jsx
+++ web/static/src/components/Map.jsx
@@ -5,13 +5,18 @@ import { onMount } from "solid-js";
 const Map = ()=>{
 
     onMount(() => {
-        const map = L.map('map-div').setView([51.505, -0.09], 4);;
+        const map = L.map('map-div').setView([51.505, -0.09], 4);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-        L.control.mousePosition().addTo(map);
+        //L.control.mousePosition().addTo(map);
+
+        let t1 = L.marker([51.505, -0.09]);
+        t1.addTo(map);
+
+        setTimeout(()=>t1.remove(), 3000);
     })
     return <div id="map-div" class="h-3/4 w-full z-20"></div>
 };
